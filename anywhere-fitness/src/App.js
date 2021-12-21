80e91bf6544dc53372bf619f3affd7bdef98646c
David Conley <dconley1989@gmail.com>
2021-12-21 11:40:58 -0700

    finished the route paths for home login and sign up pages and did the styling for the header

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,17 +1,72 @@
+import React from "react";
+import "./App.css";
+import { Link, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/LoginForm";
+import SignUp from "./components/SignUp";
+import styled from "styled-components";
 
-import React from 'react';
-import './App.css';
+const StyledHeader = styled.div`
+  display: inline-block;
+  background-color: #deb887;
+  width: 100%;
+`;
+
+const StyledLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 85%;
+`;
+
+const StyledLogin = styled.div`
+  margin: 0 4rem;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #d3d3d3;
+  }
+`;
+
+const StyledRegister = styled.div`
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #d3d3d3;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-   
-        <p>
-          Anywhere Fitness
-        </p>
-        
-      </header>
+      <StyledHeader>
+        <header className="App-header">
+          <Link to="/">
+            <h1>Anywhere Fitness</h1>
+          </Link>
+          <div className="nav-links">
+            <StyledLinks>
+              <StyledLogin>
+                <Link to="/login">Sign In</Link>
+              </StyledLogin>
+              <StyledRegister>
+                <Link to="/signup">Register</Link>
+              </StyledRegister>
+            </StyledLinks>
+          </div>
+        </header>
+      </StyledHeader>
+      <div>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </div>
     </div>
   );
 }
