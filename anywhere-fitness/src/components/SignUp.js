d2bff8d501e9c9d0f314d2c1d9fbc633c2a9695a
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-12-22 08:56:43 -0700

    working on login,logout,signup

--- anywhere-fitness/src/components/SignUp.js
+++ anywhere-fitness/src/components/SignUp.js
@@ -1,9 +1,76 @@
 import React from "react";
 
 const SignUp = () => {
+    
+    
     return(
-        <div></div>
-    )
-}
+        <form >
+        <h3>Sign Up</h3>
+        <label>
+          Username 
+          <input
+            name="username"
+            type="text"
+            //onChange={handleChange}
+            //value=
+          />
+        </label>
+        
+        <label>
+          Email 
+          <input
+            name="email"
+            type="email"
+            //onChange={handleChange}
+            //value=
+          />
+        </label>
+        
+        <label>
+          Password 
+          <input
+            name="password"
+            type="password"
+            //onChange={handleChange}
+           // value=
+          />
+        </label>
+
+        <label>
+          Client 
+          <input
+            name="client"
+            type="radio"
+           // onChange={handleChange}
+            //value=
+          />
+        </label>
+        <br />
+        <label>
+          Instructor 
+          <input
+            name="Instructor"
+            type="radio"
+            //onChange={handleChange}
+            //value=
+          />
+        </label>
+    
+          <label>
+            Authantic Code 
+            <input
+              name="auth"
+              type="text"
+              //onChange={handleChange}
+              //value=
+            />
+          </label>
+        <button>Sign Up</button>
+      </form>
+    );
+  };
 
 export default SignUp;
+
+
+
