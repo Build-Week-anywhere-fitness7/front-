5a47cb0f1a88d9965f402255e4aac176523f0aac
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-12-22 08:40:19 -0700

    working on signup

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,25 +1,80 @@
-import React from 'react';
-import './App.css';
-import Home from './components/Home';
-import { Route, Link } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Link, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import styled from "styled-components";
+import Logout from "./components/Logout";
+import PrivateRoute from "./components/PrivateRoute"
+import Class from "./components/Class"
+import AddClass from "./components/AddClass"
 
+const StyledHeader = styled.div`
+  display: inline-block;
+  background-color: #deb887;
+  width: 100%;
+`;
+
+const StyledLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 85%;
+`;
+
+const StyledLogin = styled.div`
+  margin: 0 4rem;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #d3d3d3;
+  }
+`;
+
+const StyledRegister = styled.div`
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background-color: #d3d3d3;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-   
-        <p>
-          Anywhere Fitness
-        </p>
-        
-      </header>
-
-    
-
+      <StyledHeader>
+        <header className="App-header">
+          <Link to="/">
+            <h1>Anywhere Fitness</h1>
+          </Link>
+          <div className="nav-links">
+            <StyledLinks>
+              <StyledLogin>
+                <Link to="/login">Sign In</Link>
+              </StyledLogin>
+              <StyledRegister>
+                <Link to="/signup">Register</Link>
+              </StyledRegister>
+            </StyledLinks>
+          </div>
+        </header>
+      </StyledHeader>
+      <div>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <PrivateRoute path="/login" component={Login}/>
+  
+        <PrivateRoute path="/logout" component={Logout}/>
+         <PrivateRoute path="/class/add" component={AddClass}/>
+         <PrivateRoute path="/class" component={Class}/>
+         <Route  path="/signup" component={SignUp}>
+         <SignUp />
+         </Route>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App;;
--- anywhere-fitness/src/components/Logout.js
+++ anywhere-fitness/src/components/Logout.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 const Logout = () => {
+    //const {push} = useHistory()
+
+   // useEffect(()=> {
+      //  axiosWithAuth()
+          //  .post('/logout')
+           // .then(resp => {
+              //  localStorage.removeItem('token');
+               // push('/login');
+          //  });
+           
+   // }, [])    
     return(
         <div></div>
     )
