e7f8622e883535718a8a7de2030b5fe73d068095
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-12-22 10:26:16 -0700

    working on forms

--- anywhere-fitness/src/components/AddClass.js
+++ anywhere-fitness/src/components/AddClass.js
@@ -1,9 +1,103 @@
-import React from "react";
+import React, {useState} from 'react';
 
-const AddClass = () => {
+
+
+
+const AddClass = props => {
+    const [item, setItem] = useState( {
+        name: "",
+        class_type_name: "",
+        start_time: "",
+        duration: "",
+        intensity: "",
+        location: "",
+        current_number_registrants: "",
+        max_class_size: ""
+    })
+    
     return(
-        <div></div>
+        <form >
+               <label>
+               Class Name
+               <input
+               name="class_name"
+               //value=
+               //onChange={handleChange}
+
+               />
+               </label>
+               <label>
+               Class Type
+               <input
+               name="class_type_name"
+               //value=
+               //onChange={handleChange}
+
+               />
+               </label>
+              
+               <label>
+               Class Time
+               <input
+               name="class_time"
+              // value=
+               //onChange={handleChange}
+
+               />
+               </label>
+               <label>
+               Class Duration
+               <input
+               name="duration"
+               //value=
+               //onChange={handleChange}
+
+               />
+               </label>
+               <label>
+               Intensity Level
+               <input
+               name="intensity"
+               //value=
+              // onChange={handleChange}
+
+               />
+               </label>
+               <label>
+               Class Location
+               <input
+               name="location"
+               //value=
+              // onChange={handleChange}
+
+               />
+               </label>
+             
+              <label>
+              Current Number Registrants
+               <input
+               name="current_number_registrants"
+               //value=
+               //onChange={handleChange}
+
+               />
+               </label>
+
+                 <label>
+                 Max Class Size
+               <input
+               name="max_class_size"
+               //value=
+               //onChange={handleChange}
+
+               />               
+               </label>
+            <button>Add Class</button>
+            
+           </form>
+
     )
 }
+
 
 export default AddClass;
