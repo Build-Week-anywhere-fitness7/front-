0f93db66d569e717db59e829a4548f2415760f54
MasterHap <jjhappel4213@gmail.com>
2021-12-23 06:29:38 -0500

    Update AddClass.js

--- anywhere-fitness/src/components/AddClass.js
+++ anywhere-fitness/src/components/AddClass.js
@@ -1,55 +1,25 @@
 import React, {useState} from 'react';
-import { useHistory } from 'react-router-dom';
 import axios from 'axios'
-import axiosWithAuth from './utils/axiosWithAuth'
-
-const initialFormValues = {
-    class_Id: "",
-}
 
 const AddClass=() => {
-    const [formValues, setFormValues] = useState(initialFormValues)
-
-    const {push} = usehistory()
-
-    const postNewItem = newItem => {
-        console.log("post Items=", newItem)
-        axiosWithAuth().post('', newItem)
-            .then(response => {
-                console.log(response);
-                push('/Class');
-            })
-            .catch(error => {
-                console.error(error)
-            })
-            .finally(() => {
-                setFormValues(initialFormValues)
-            })
+    const [workout, setWorkout] = useState({
+        class_Id:"",
+    })
+    const onChange = (e) => {
+        setWorkout({
+            ...workout,[e.target.class_Id]: e.target.value,
+        });
     }
-
-    const submitItem = () => {
-        const newItem = {
-            class_Id: formValues.class_Id,
-        }
-        postNewItem(newItem)
+    const onSubmit = (e) => {
+        e.preventDefault();
+        
     }
-    const onSubmit = event => {
-        event.preventDefault()
-        submitItem()
-    }
-    
-    const onChange = event => {
-        setFormValues({
-            ...formValues,
-            [event.target.name]: event.target.value,
-        })
-    }
-
     return(
         <form >
                <label>
-                <h1>Choose Your Class!!</h1>
+                <h1>Choose Your Class</h1>
                     <select id="class_Id" onChange={onChange}>
+                        <option>Choose Class</option>
                         <option value="1">Morning Zen</option>
                         <option value="2">Boxing Basics</option>
                         <option value="3">Sunday Spinning</option>
