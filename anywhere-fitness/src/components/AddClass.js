3c13957b8112568d7616f5b231e5d19d7a13e81a
MasterHap <jjhappel4213@gmail.com>
2021-12-23 05:42:12 -0500

    Update AddClass.js

--- anywhere-fitness/src/components/AddClass.js
+++ anywhere-fitness/src/components/AddClass.js
@@ -4,43 +4,32 @@ import axios from 'axios'
 import axiosWithAuth from './utils/axiosWithAuth'
 
 const initialFormValues = {
-    class_name: "",
-    class_type_name: "",
-    class_time: "",
-    duration: "",
-    intensity: "",
-    location: "",
-    current_number_registrants: "",
-    max_class_size: ""
+    class_Id: "",
 }
-
-const initialFormErrors = {
-    name: "",
-}
-
-const initialDisabled = true
 
 const AddClass=() => {
     const [formValues, setFormValues] = useState(initialFormValues)
-    const [formErrors, setFormErrors] = useState(initialFormErrors)
-    const [disabled, setDisabled] = useState(initialDisabled)
 
     const {push} = usehistory()
 
     const postNewItem = newItem => {
-
+        console.log("post Items=", newItem)
+        axiosWithAuth().post('', newItem)
+            .then(response => {
+                console.log(response);
+                push('/Class');
+            })
+            .catch(error => {
+                console.error(error)
+            })
+            .finally(() => {
+                setFormValues(initialFormValues)
+            })
     }
 
     const submitItem = () => {
         const newItem = {
-            class_name: formValues.class_name,
-            class_type_name: formValues.class_type_name,
-            class_time: formValues.class_time,
-            duration: formValues.duration,
-            intensity: formValues.intensity,
-            location: formValues.location,
-            current_number_registrants: formValues.current_number_registrants,
-            max_class_size: formValues.max_class_size
+            class_Id: formValues.class_Id,
         }
         postNewItem(newItem)
     }
@@ -59,85 +48,17 @@ const AddClass=() => {
     return(
         <form >
                <label>
-               Class Name
-               <input
-               name="class_name"
-               value={formValues.class_name}
-               onChange={onChange}
-
-               />
+                <h1>Choose Your Class!!</h1>
+                    <select id="class_Id" onChange={onChange}>
+                        <option value="1">Morning Zen</option>
+                        <option value="2">Boxing Basics</option>
+                        <option value="3">Sunday Spinning</option>
+                        <option value="4">Water Aerobics</option>
+                        <option value="5">Bikram Yoga</option>
+                    </select>
                </label>
-               <label>
-               Class Type
-               <input
-               name="class_type_name"
-               value={formValues.class_type_name}
-               onChange={onChange}
-
-               />
-               </label>
-              
-               <label>
-               Class Time
-               <input
-               name="class_time"
-               value={formValues.class_time}
-               onChange={onChange}
-
-               />
-               </label>
-               <label>
-               Class Duration
-               <input
-               name="duration"
-               value={formValues.duration}
-               onChange={onChange}
-
-               />
-               </label>
-               <label>
-               Intensity Level
-               <input
-               name="intensity"
-               value={formValues.intensity}
-               onChange={onChange}
-
-               />
-               </label>
-               <label>
-               Class Location
-               <input
-               name="location"
-               value={formValues.location}
-               onChange={onChange}
-
-               />
-               </label>
-             
-              <label>
-              Current Number Registrants
-               <input
-               name="current_number_registrants"
-               value={formValues.current_number_registrants}
-               onChange={onChange}
-
-               />
-               </label>
-
-                 <label>
-                 Max Class Size
-               <input
-               name="max_class_size"
-               value={formValues.max_class_size}
-               onChange={onChange}
-
-               />               
-               </label>
-            <button
-            onSubmit={onSubmit}
-            >Add Class</button>
-           </form>
-
+            <button id="btn" onSubmit={onSubmit}>Add Class</button>
+        </form>
     )
 }
 
